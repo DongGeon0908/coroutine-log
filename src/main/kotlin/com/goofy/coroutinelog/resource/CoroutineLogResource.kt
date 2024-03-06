0ad72f43ce2c04de245bd6f8a501201d6efe9e21
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2024-03-06 10:45:03 +0900

    feat: test 환경 구성

--- src/main/kotlin/com/goofy/coroutinelog/resource/CoroutineLogResource.kt
+++ src/main/kotlin/com/goofy/coroutinelog/resource/CoroutineLogResource.kt
@@ -1,8 +1,13 @@
 package com.goofy.coroutinelog.resource
 
+import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RestControllerAdvice
 import java.time.LocalDateTime
 
 @RestController
@@ -26,7 +31,9 @@ class CoroutineLogResource(
 }
 
 @Service
-class CoroutineLogService {
+class CoroutineLogService(
+    private val testDataMakerService: TestDataMakerService
+) {
     fun getV1(): Response {
         return Response(
             message = "test1",
@@ -49,8 +56,44 @@ class CoroutineLogService {
     }
 }
 
+@Service
+class TestDataMakerService {
+    suspend fun test1(): String {
+        return withContext(Dispatchers.IO) { "hello data" }
+    }
+
+    suspend fun test2(): LocalDateTime {
+        return withContext(Dispatchers.IO) { LocalDateTime.now() }
+    }
+
+    suspend fun test3(): Map<String, Any> {
+        return withContext(Dispatchers.IO) {
+            mutableMapOf(
+                "test1" to "String",
+                "test2" to "LocalDateTime",
+                "test3" to "Map"
+            )
+        }
+    }
+}
+
 data class Response(
     val message: String,
     val dateTime: LocalDateTime,
     val extra: Map<String, Any> = emptyMap()
+)
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+    private val logger = KotlinLogging.logger { }
+
+    @ExceptionHandler(Exception::class)
+    fun handle(e: Exception): ErrorResponse {
+        logger.error(e) { "Exception." }
+        return ErrorResponse(e.message ?: e.stackTraceToString())
+    }
+}
+
+data class ErrorResponse(
+    val message: String
 )
